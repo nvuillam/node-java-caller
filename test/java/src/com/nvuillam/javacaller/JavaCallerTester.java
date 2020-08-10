eaa30b1206a9ee1ffb051cf8feb794c3d0d586b4
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-08-10 10:29:12 +0200

    Return javaChildProcess property
    - Return `javaChildProcess` property when `detached` is true, so it can be used to be killed later

--- test/java/src/com/nvuillam/javacaller/JavaCallerTester.java
+++ test/java/src/com/nvuillam/javacaller/JavaCallerTester.java
@@ -1,10 +1,22 @@
 package com.nvuillam.javacaller;
 
+import java.lang.InterruptedException;
+import java.util.concurrent.TimeUnit;
+
 public class JavaCallerTester
 {
     public static void main(String[] args)
     {
         System.out.println("JavaCallerTester is called !");
         System.out.println(java.util.Arrays.toString(args));
+        if (args.length > 0 && args[0] != null && args[0] == "--sleep") {
+            try {
+                TimeUnit.MINUTES.sleep(1);
+            } catch (InterruptedException eInterrupt) {
+                System.err.println("JavaCallerTester interrupted !");
+            } catch (Throwable t) {
+                System.err.println("JavaCallerTester crashed !");
+            }
+        }
     }
 }
